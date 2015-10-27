4026e7c1be96dacd2e0162c93313c3d37078a1ba
khangal <hangal2001@gmail.com>
2015-10-26 23:12:27 -0500

    added timer on UI

---
+++ BushBurg/Assets/timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+     public Text timerLabel;
+ 
+     private float time;
+ 
+     void LateUpdate() {
+
+         time += Time.deltaTime;
+
+         var hours = time/3600;
+         var minutes = (time / 60) % 60;
+         var seconds = time % 60;
+
+         timerLabel.text = string.Format ("{0:00} : {1:00} : {2:00}", hours, minutes, seconds);
+     }
+ }
